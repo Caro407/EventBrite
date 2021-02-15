7075401818b0d385f106e5cbbf9b798c7df05982
Caro407 <caro.lefevre407@hotmail.fr>
2021-02-15 17:34:44 +0100

    setting up mailer for new users

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
 
   has_many :attendances
   has_many :events, through: :attendances
+
+  after_create :send_welcome_email
+
+  def send_welcome_email
+    UserMailer.welcome_email(self).deliver_now
+  end
 end
