aa0392c1f2bb67a427c35734ea22ccf323aa9a5d
Caro407 <caro.lefevre407@hotmail.fr>
2021-02-18 16:49:58 +0100

    review admin rights to modify event

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,6 @@
 class EventsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
+  before_action :is_user_admin?, only: [:edit, :update]
 
   def index
   end
@@ -34,6 +35,41 @@ class EventsController < ApplicationController
     else
       flash[:danger] = "Echec lors de la création de l'évènement : " + @event.errors.full_messages.join(" ")
       render :new
+    end
+  end
+
+  def edit
+    @event = Event.find(params[:id])
+  end
+
+  def update_params
+    params.permit(:id, :title, :description, :start_date, :duration, :price)
+  end
+
+  def update
+    @event = Event.find(update_params[:id])
+
+    @event.title = update_params[:title]
+    @event.description = update_params[:description]
+    @event.start_date = update_params[:start_date]
+    @event.duration = update_params[:duration]
+    @event.price = update_params[:price]
+
+    if @event.save
+      redirect_to event_path(@event.id)
+      flash[:success] = "Les informations ont été correctement mises à jour."
+    else
+      flash[:danger] = "Echec : " + @event.errors.full_messages.join(" ")
+      render :edit
+    end
+  end
+
+  private
+
+  def is_user_admin?
+    if Event.find(params[:id]).host != helpers.current_user
+      redirect_to root_path
+      flash[:warning] = "Vous ne pouvez pas modifier un évènement sans en être l'administrateur."
     end
   end
 end
